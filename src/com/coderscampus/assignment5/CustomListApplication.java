40c097c597c47cbf59e65cccbdb4f5674774658e
Lucas Becker <lucasbecker.dev@gmail.com>
2024-03-20 14:36:35 -0400

    clarified printing

--- src/com/coderscampus/assignment5/CustomListApplication.java
+++ src/com/coderscampus/assignment5/CustomListApplication.java
@@ -3,22 +3,19 @@ package com.coderscampus.assignment5;
 public class CustomListApplication {
     public static void main(String[] args) {
         CustomList<String> customListString = new CustomArrayList<>();
-        CustomList<Integer> customListInteger = new CustomArrayList<>();
 
         for (int i = 0; i < 50; i++) {
             customListString.add("element " + i);
         }
 
-        for (int i = 0; i < 100; i++) {
-            customListInteger.add(i);
+        // test print via for loop
+        for (int i = 0; i < customListString.getSize(); i++) {
+            System.out.println(customListString.get(i));
         }
 
-//        for (int i = 0; i < customListString.getSize(); i++) {
-//            System.out.println(customListString.get(i));
-//        }
+        // test print via stream.forEach
         customListString.stream()
                         .forEach(System.out::println);
-        customListInteger.stream()
-                         .forEach(System.out::println);
+
     }
 }
