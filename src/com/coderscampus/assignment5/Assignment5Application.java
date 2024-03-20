6399ca11e4a50ace91f46b28745c7082d092b03a
Lucas Becker <lucasbecker.dev@gmail.com>
2024-03-20 12:17:33 -0400

    project setup

--- src/com/coderscampus/assignment5/Assignment5Application.java
+++ src/com/coderscampus/assignment5/Assignment5Application.java
@@ -1,4 +1,6 @@
-public class Main {
+package com.coderscampus.assignment5;
+
+public class Assignment5Application {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
